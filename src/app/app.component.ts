e244da15ff24e9602471be6c50dcaffc67e42513
Milele Fushi <milele.fushi@gmail.com>
2025-03-04 19:06:47 +0100

    Adjust README and gitignore. Clean project initial data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'gl-internship-25';
-}
+export class AppComponent {}
